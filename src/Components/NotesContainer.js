cffef0fb0751e2674679bd9df6d62352aa32e254
Luis Reyes <luisreyesxv@yahoo.com>
2020-08-24 10:16:07 -0400

    i have no idea if this worked. npm is giving me issues, i'm searching to see how to resolve it

--- src/Components/NotesContainer.js
+++ src/Components/NotesContainer.js
@@ -1,13 +1,18 @@
 import React from 'react'
-
+import Note from './Note'
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
         return ["First Note", "Second Note", "Third Note", "Fourth Note"]
     }
+    let notes = apiResponse.map(function (note){Note(note)}
 
-    return(
-        <ul></ul >
+    return( 
+        <div>
+
+            <ul>{notes}</ul >
+
+            </div>
     )
 
 }
