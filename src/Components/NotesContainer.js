1d3bf221872c631ca0075ff91693475c4a43e42c
hahm-d <dhahm01@gmail.com>
2020-08-24 10:09:08 -0400

    attemp1

--- src/Components/NotesContainer.js
+++ src/Components/NotesContainer.js
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import Note from './Note.js'
 class NotesContainer extends React.Component {
 
     apiResponse = () => {
         return ["First Note", "Second Note", "Third Note", "Fourth Note"]
     }
+    
+    noteList = () => {
+       return this.apiReponse().map(Note => <li>Note</li>)
+    }
 
-    return(
-        <ul></ul >
-    )
+    render() {
+        return(<ul>{this.noteList()}</ul>)
+    }
 
 }
+
+export default NotesContainer;
