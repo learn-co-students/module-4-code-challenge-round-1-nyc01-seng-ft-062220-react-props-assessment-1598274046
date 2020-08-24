914125b4f6f88bdee8f0b119b0fc167aa7851833
scubaguba345 <brandon.gubitosa@gmail.com>
2020-08-24 11:38:52 -0400

    sorry about that didnt push up first time

--- src/Components/NotesContainer.js
+++ src/Components/NotesContainer.js
@@ -1,4 +1,6 @@
+
 import React from 'react'
+import Note from './Note.js'
 
 class NotesContainer extends React.Component {
 
@@ -6,8 +8,27 @@ class NotesContainer extends React.Component {
         return ["First Note", "Second Note", "Third Note", "Fourth Note"]
     }
 
-    return(
-        <ul></ul >
-    )
+    render() {
+      //need to map over the api response here ran out of time
+        return(
+            <div>
+                <ul>
+                    <li>
+                    
+                    </li>
+                </ul >
 
+            </div>
+            
+        )
+    }
 }
+
+export default NotesContainer;
+
+
+// The NotesContainer component has an apiResponse method that returns an array of notes
+
+// Each Note component should be able to access the content key inside of its props and print the value of the content key to the DOM as a li tag
+
+// The NotesContainer should be rendered underneath the Header on the DOM
