21e1c8b57f9dffbf62f2eac038a40e3df32bd9f2
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-24 10:03:35 -0400

    final commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-        <h1>{"This is a mod 4 code challenge!"}</h1>
+        <h1 className={"App-header"}>{"This is a mod 4 code challenge!"}</h1>
 
   )
   }
